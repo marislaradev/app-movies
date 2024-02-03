41839e53fe06d62a7d72e9508e37e9c7c6a21e73
Mariana da Silva Lara <marislaradev@outlook.com>
2024-02-03 13:52:09 -0300

    feat: add Category component and filter videos by category

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import Banner from "./components/Banner";
 import Card from "./components/Card";
+import Category from "./components/Category";
 import Container from "./components/Container";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import videos from "./json/db.json";
+import videos from "./json/videos.json";
+
+const categories = [
+  "Muamba de Rima",
+  "Dois lados da moeda"
+]
+
+function filterCategory(id) {
+  return videos.filter( video => video.category === categories[id])
+}
 
 function App() {
   return (
@@ -11,12 +21,19 @@ function App() {
       <Header />
       <Banner image="favoritos" />
       <Container>
-        <h2>Muamba de Rima</h2>
-        <section className="cards">
-          {videos.map((video) => {
-            return <Card id={video.id} key={video.id} />;
-          })}
-        </section>
+
+        <Category category={categories[0]}>
+          {filterCategory(0).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
+        <Category category={categories[1]}>
+          {filterCategory(1).map((video) => (
+            <Card id={video.id} key={video.id} />
+          ))}
+        </Category>
+
       </Container>
       <Footer />
     </>
