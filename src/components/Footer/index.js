5cea75469455745588245a65ae610061e705019d
Mariana da Silva Lara <marislaradev@outlook.com>
2024-02-03 12:06:57 -0300

    chore: initialize React project and add initial components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <h2 className={styles.footer}>MariFlix &copy; Desenvolvido por Mariana da Silva Lara</h2>
+    );
+}
+
+export default Footer;
