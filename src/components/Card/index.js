eb1704317802b16bcee273333d2d33444a2f40e5
Mariana da Silva Lara <marislaradev@outlook.com>
2024-02-15 14:36:31 -0300

    feat: create Favorites page and route

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
+import iconFavorite from "./favorite.png";
+import iconUnfavorite from "./unfavorite.png";
 
 function Card({ id }) {
     return(
         <section className={styles.card}>
             <Link 
             to={`/watch/${id}`}>
-                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}alt="Capa"/>
+                <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}alt="Capa" className={styles.cover}/>
             </Link>
+            <figure className={styles.icon}>
+                <img src={iconUnfavorite} alt="Ícone favoritos"/>
+            </figure>
         </section>
     );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,6 +11,7 @@ function Header() {
       <nav>
         <Link to="/">Home</Link>
         <Link to="/search">Pesquisar</Link>
+        <Link to="/favorites">Favoritos</Link>
       </nav>
     </header>
   );
