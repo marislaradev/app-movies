29b1a93380cc0faff5d192b77f9a7b3d6d163468
Mariana da Silva Lara <marislaradev@outlook.com>
2024-02-16 10:53:24 -0300

    feat: create contexts, FavoriteContext, and custom hook, adjust routes and Cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import styles from "./Card.module.css";
-import iconFavorite from "./favorite.png";
-import iconUnfavorite from "./unfavorite.png";
+import iconFavorite from "./favorite.png"; //branco
+import iconUnfavorite from "./unfavorite.png"; //vermelho
+import { useFavoriteContext } from "../../contexts/Favorites";
 
 function Card({ id }) {
+
+    const { favorite, addFavorite } = useFavoriteContext()
+    const isFavorite = favorite.some((fav) => fav.id === id)
+    const toggleIcon = !isFavorite ? iconFavorite : iconUnfavorite
+
     return(
         <section className={styles.card}>
             <Link 
@@ -11,7 +17,10 @@ function Card({ id }) {
                 <img src={`https://img.youtube.com/vi/${id}/mqdefault.jpg`}alt="Capa" className={styles.cover}/>
             </Link>
             <figure className={styles.icon}>
-                <img src={iconUnfavorite} alt="Ícone favoritos"/>
+                <img 
+                    src={toggleIcon} 
+                    alt="Ícone favoritos"
+                    onClick={() => addFavorite({id})}/>
             </figure>
         </section>
     );
